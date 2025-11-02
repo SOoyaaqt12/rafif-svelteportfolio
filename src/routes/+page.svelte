122c9b9976119a6694cb4c33cc89dd49e4b55e18
SOoyaaqt12 <daffa.rafif364@smk.belajar.id>
2025-11-02 20:39:32 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-6
+xl">haloo semuanya</h1>
+<p>perkenalkan saya daffa</p>
+<p>saya sedang belajar sveltekit</p>
+<p>sveltekit itu keren banget</p>
+<p>ayo belajar bersama saya</p>
+<p>terima kasih sudah berkunjung</p>
+<a href="/about">About My Website</a>
